9157dfb292cb055eb3b0238b2fc6e995202f111c
DilanLivera <DilanLivera@users.noreply.github.com>
2019-07-02 16:39:09 +1000

    update middlewear for errors

--- app.js
+++ app.js
@@ -14,6 +14,22 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+//middlewear for errors
+app.use((req, res, next) => {
+  const error = new Error("Page Not Found!!!");
+  error.status(404);
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500)
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
 // listen for requests :)
 const listener = app.listen(PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
